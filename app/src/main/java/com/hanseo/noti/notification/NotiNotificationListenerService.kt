fac2e6b76b7ccce7f53ca96112d60e28e019450a
Shin Han Seo <imkara1@naver.com>
2026-07-23 18:58:12 +0900

    feat: 알림 수신 콜백 추가

--- app/src/main/java/com/hanseo/noti/notification/NotiNotificationListenerService.kt
+++ app/src/main/java/com/hanseo/noti/notification/NotiNotificationListenerService.kt
@@ -2,14 +2,24 @@ package com.hanseo.noti.notification
 
 import android.service.notification.NotificationListenerService
 import android.util.Log
+import android.service.notification.StatusBarNotification
 
 class NotiNotificationListenerService :
     NotificationListenerService() {
 
-    override fun onListenerConnected() {
+    override fun onListenerConnected() { // 서비스 연결 함수
         super.onListenerConnected()
 
         Log.d(TAG, "Notification listener connected")
+    }
+
+    override fun onNotificationPosted( // 알림 도착 시 콜백 확인 함수
+        sbn: StatusBarNotification?
+    ) {
+        if (sbn == null) return // 알람이 없으면 return
+        if (sbn.packageName == packageName) return // noti. 자기 앱일 경우 return
+
+        Log.d(TAG, "Notification received")
     }
 
     companion object {
