dca4919d85834004eb8d40db43caa0079f3922b9
Shin Han Seo <imkara1@naver.com>
2026-07-26 16:12:44 +0900

    feat: Noti Database abstact class 추가

---
+++ app/src/main/java/com/hanseo/noti/data/local/NotiDatabase.kt
@@ -0,0 +1,16 @@
+package com.hanseo.noti.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.hanseo.noti.data.local.dao.NotificationDao
+import com.hanseo.noti.data.local.entity.NotificationEntity
+
+@Database(
+    entities = [NotificationEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NotiDatabase : RoomDatabase() {
+
+    abstract fun notificationDao(): NotificationDao
+}
