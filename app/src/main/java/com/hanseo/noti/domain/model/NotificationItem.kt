ba4b8e745539d206db1b53e6b54809a36e3238c1
Shin Han Seo <imkara1@naver.com>
2026-07-26 16:58:47 +0900

    feat: 제거 여부 및 제거 시각 추가

--- app/src/main/java/com/hanseo/noti/domain/model/NotificationItem.kt
+++ app/src/main/java/com/hanseo/noti/domain/model/NotificationItem.kt
@@ -8,6 +8,8 @@ data class NotificationItem(
     val postedAt: Long, // 시각
     val category: String?, // 메시지, 전화, 일정 등 Android가 지정한 알림 종류
     val isOngoing: Boolean, // 다운로드 음악 재생처럼 계속 유지되는 알람인지 여부
-    val isGroupSummary: Boolean // 여러 알림을 묶어 보여주는 대표 알림인지 여부
+    val isGroupSummary: Boolean, // 여러 알림을 묶어 보여주는 대표 알림인지 여부
+    val isRemoved: Boolean = false, // Soft Delete
+    val removedAt: Long? = null // 삭제 시각
 )
 
