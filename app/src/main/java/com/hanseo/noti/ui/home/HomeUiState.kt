851c8b68d29a0a085bda80c149430198840563af
Shin Han Seo <imkara1@naver.com>
2026-07-30 15:53:03 +0900

    fix: 기존 알림 단수에서 리스트로 변경

--- app/src/main/java/com/hanseo/noti/ui/home/HomeUiState.kt
+++ app/src/main/java/com/hanseo/noti/ui/home/HomeUiState.kt
@@ -3,6 +3,6 @@ package com.hanseo.noti.ui.home
 import com.hanseo.noti.domain.model.ClassifiedNotification
 
 data class HomeUiState(
-    val importantNotification: List<ClassifiedNotification>
+    val importantNotifications: List<ClassifiedNotification> = emptyList()
 )
 
