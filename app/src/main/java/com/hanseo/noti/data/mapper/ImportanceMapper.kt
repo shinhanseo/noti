53963f7dbd2563104f491bd17849bcce5018689d
Shin Han Seo <imkara1@naver.com>
2026-07-30 14:14:53 +0900

    refacotr: 파일 위치 수정

--- app/src/main/java/com/hanseo/noti/data/mapper/ImportanceMapper.kt
+++ app/src/main/java/com/hanseo/noti/data/mapper/ImportanceMapper.kt
@@ -1,5 +1,6 @@
-package com.hanseo.noti.domain.importance
+package com.hanseo.noti.data.mapper
 
+import com.hanseo.noti.domain.importance.ImportanceInput
 import com.hanseo.noti.domain.model.NotificationItem
 
 fun NotificationItem.toImportanceInput(): ImportanceInput {
