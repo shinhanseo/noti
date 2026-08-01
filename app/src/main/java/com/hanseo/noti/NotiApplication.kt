1a2ef388d5d6edc03d3685d5c7c3a55e421f1d2b
Shin Han Seo <imkara1@naver.com>
2026-08-01 18:59:06 +0900

    refacotr: hilt 추가에 따른 코드 리팩토링

--- app/src/main/java/com/hanseo/noti/NotiApplication.kt
+++ app/src/main/java/com/hanseo/noti/NotiApplication.kt
@@ -1,31 +1,7 @@
 package com.hanseo.noti
 
 import android.app.Application
-import androidx.room.Room
-import com.hanseo.noti.data.local.NotiDatabase
-import com.hanseo.noti.data.repository.NotificationRepository
-import com.hanseo.noti.data.local.migration.MIGRATION_1_2
-import com.hanseo.noti.data.local.migration.MIGRATION_2_3
-import com.hanseo.noti.data.local.migration.MIGRATION_3_4
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NotiApplication : Application() {
-    val database: NotiDatabase by lazy {
-        Room.databaseBuilder(
-            this,
-            NotiDatabase::class.java,
-            DATABASE_NAME
-        )
-            .addMigrations(
-                MIGRATION_1_2,
-                MIGRATION_2_3,
-                MIGRATION_3_4
-            )
-            .build()
-    }
-
-    companion object {
-        private const val DATABASE_NAME = "noti.db"
-    }
-}
+class NotiApplication : Application()
