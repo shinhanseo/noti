203e3e0138d483c9777caca5498a5e593bef97d2
Shin Han Seo <imkara1@naver.com>
2026-08-02 18:00:30 +0900

    feat: 바텀바 관련 코드 추가

--- app/src/main/java/com/hanseo/noti/ui/navigation/NotiRoutes.kt
+++ app/src/main/java/com/hanseo/noti/ui/navigation/NotiRoutes.kt
@@ -1,6 +1,13 @@
 package com.hanseo.noti.ui.navigation
 
 object NotiRoutes {
+
+    // 바텀 탭이 없는 최상위 화면
     const val ONBOARDING = "onboarding"
+    const val MAIN = "main"
+
+    // MainScreen 내부 바텀 탭 화면
     const val HOME = "home"
+    const val NOTIFICATIONS = "notifications"
+    const val MY_CRITERIA = "my_criteria"
 }
