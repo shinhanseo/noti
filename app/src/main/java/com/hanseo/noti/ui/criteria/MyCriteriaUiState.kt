86ac985795ff6dba2f38be363fee320078057c8f
Shin Han Seo <imkara1@naver.com>
2026-08-11 12:56:58 +0900

    feat: 내 기준 Ui State 추가

---
+++ app/src/main/java/com/hanseo/noti/ui/criteria/MyCriteriaUiState.kt
@@ -0,0 +1,7 @@
+package com.hanseo.noti.ui.criteria
+
+data class MyCriteriaUiState (
+    val importantAppCount: Int = 0,
+    val importantKeywordCount: Int = 0,
+    val exclusionKeywordCount: Int = 0,
+)
