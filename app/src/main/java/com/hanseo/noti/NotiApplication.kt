b47789234534e53d127bebffb805c57d9ce5932c
Shin Han Seo <imkara1@naver.com>
2026-08-26 12:53:37 +0900

    feat: work manager 실제 호출ㄹ

--- app/src/main/java/com/hanseo/noti/NotiApplication.kt
+++ app/src/main/java/com/hanseo/noti/NotiApplication.kt
@@ -3,6 +3,7 @@ package com.hanseo.noti
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.hanseo.noti.notification.work.NotificationListenerRecoveryScheduler
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -14,9 +15,19 @@ class NotiApplication :
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
+    @Inject
+    lateinit var recoveryScheduler:
+            NotificationListenerRecoveryScheduler
+
     override val workManagerConfiguration: Configuration
         get() =
             Configuration.Builder()
                 .setWorkerFactory(workerFactory)
                 .build()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        recoveryScheduler.schedule()
+    }
 }
