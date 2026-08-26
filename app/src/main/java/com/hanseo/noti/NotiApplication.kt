477f30375b7ceb61f9738fb5162617f0687a7894
Shin Han Seo <imkara1@naver.com>
2026-08-26 12:47:05 +0900

    feat: Hilt 기반 WorkManager 설정 추가

--- app/src/main/java/com/hanseo/noti/NotiApplication.kt
+++ app/src/main/java/com/hanseo/noti/NotiApplication.kt
@@ -1,7 +1,22 @@
 package com.hanseo.noti
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NotiApplication : Application()
+class NotiApplication :
+    Application(),
+    Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() =
+            Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .build()
+}
